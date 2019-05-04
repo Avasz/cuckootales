17b291e307364ecbc06437a65ccf6dca0a865c7d
Avasz <avasz@x260>
2019-05-04 16:34:10 +0545

    initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# About - Cuckoo Tales
+
+Simple bash script that downloads and extracts mp3 from youtube videos and uploads it to a Telegram channel.
+
+Telegram can be used as a music player substitute. It is kind of cool and handy feature of Telegram.
+
+License: WTFPL ![WTFPL LOGO](http://www.wtfpl.net/wp-content/uploads/2012/12/wtfpl-badge-1.png)
+
+# Dependencies
+
+1. [youtube-dl](https://github.com/ytdl-org/youtube-dl)  
+2. ffmpeg  
+
+It uses `youtube-dl` to download and extract audio from youtube videos. It also requires `ffmpeg` for the conversion.
+
+# Usage
+
+1. Create a Telegram bot using Botfather. Details can be found [here](https://core.telegram.org/bots#3-how-do-i-create-a-bot]).  
+2. Create a Telegram channel or group and get the chat id.  
+3. If you are using a channel, make the bot administrator of that channel.  
+4. Configure the script with bot-token and chat ID.  
+
+It is simple to use:  
+`./cuckoo.sh <youtube-link-here>`  
+
+# [Subscribe to my channel here](https://t.me/cuckootales)
+
---
+++ cuckoo.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+# Author: Avash Mulmi <avasz@protonmail.com>
+# Fun script to create music player in telegram with telegram channel
+# This downloads songs from youtube, extracts audio and uploads it to a telegram channel.
+# Required: youtube-dl, telegram bot and bot ID, telegram channel or group and its ID
+# Comes without any warranty, you are responsible for any damage this script may cause... 
+# License: WTFPL  <http://www.wtfpl.net/>
+
+chat_id=""
+bot_token=""
+
+YOUTUBEDL="/usr/local/bin/youtube-dl"
+echo "Downloading song info...."
+
+INFO=`$YOUTUBEDL --get-filename -o '%(title)s:%(alt_title)s:%(artist)s:%(genre)s:%(album)s:%(track)s:%(uploader)s:%(creator)s:%(channel)s:%(duration)s' $1`
+echo $INFO
+
+title=$(echo $INFO | cut -f 1 -d":" | sed  "s/ /_/g")
+alt_title=$(echo $INFO | cut -f 2 -d":")
+performer=$(echo $INFO | cut -f 3 -d":")
+genre=$(echo $INFO | cut -f 4 -d":")
+album=$(echo $INFO | cut -f 5 -d":")
+track=$(echo $INFO | cut -f 6 -d":")
+uploader=$(echo $INFO | cut -f 7 -d":")
+duration=$(echo $INFO | cut -f 10 -d":")
+
+echo "Downloading music...."
+$YOUTUBEDL -x --audio-format mp3 -o /tmp/$title."%(ext)s" --write-thumbnail $1
+
+echo "Uploading to telegram..."
+curl -X POST https://api.telegram.org/bot$bot_token/sendAudio -F chat_id="$chat_id" -F thumb="@/tmp/$title.jpg" -F audio="@/tmp/$title.mp3" -F caption="$title" -F title="$title" -F performer="$performer" -F duration="$duration"
+
+
+echo -e "\n\nUpload done"
+echo "Cleaning files from temporary directory..."
+rm -rf /tmp/*.mp3
+echo "Completed..."
